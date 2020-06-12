a585c953ddf3266bf36a87b22792a58025f180da
Garrett Landen <glanden@uvm.edu>
2020-06-12 14:39:34 +0000

    add Translation to getData()

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,3 +1,5 @@
+
+
 // Copyright 2019 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,25 +13,23 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-
 /**
- * Fetches message from the server and adds it to the DOM.
+ * Fetches message from the server and adds it to the DOM with translation.
  */
 function getData() {
   console.log('Fetching data.');
-
+  const languageCode = document.getElementById('language').value;
   // The fetch() function returns a Promise because the request is asynchronous.
-  const responsePromise = fetch('/data').then(response => response.json())
+  const responsePromise = fetch('/list-comments?language='+languageCode).then(response => response.json())
   .then((comments) => { 
   const commentsListElement = document.getElementById('server-comments-container');
-    commentsListElement.innerHTML = '';
+  commentsListElement.innerHTML = '';
+  var i;
+  for(i = 0; i<comments.length; i++){
     commentsListElement.appendChild(
-        createListElement('Comments: ' + comments));
-});
-
-  // When the request is complete, pass the response into handleResponse().
-  responsePromise.then(addQuoteToDom);
+    createListElement(comments[i]));
+  }
+  });
 }
 /** Creates a list element that displays comment. */
 function createListElement(comment) {
